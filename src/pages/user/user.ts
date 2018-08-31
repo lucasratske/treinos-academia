c362544c349a4c0f529a25bf8ea06a4e88f80dc1
Lucas Ratske <lucas.ratske@gmail.com>
2018-08-30 21:40:27 -0300

    Adicionado editar usuario

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,6 +1,7 @@
 import { User } from './../../models/user';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, AlertController } from 'ionic-angular';
+import { MongoService } from '../../providers/mongo/mongo.service';
 
 @IonicPage()
 @Component({
@@ -13,9 +14,89 @@ export class UserPage {
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController,
+    private mongoService: MongoService
   ) {
     this.user = <User>this.navParams.get("user");
   }
 
+  openAlertEdit() {
+    let user: User = this.user;
+    let alert = this.alertCtrl.create({
+      title: `Editar usuário`,
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Nome',
+          value: user.name
+        },
+        {
+          name: 'password',
+          placeholder: 'Senha',
+          value: user.password,
+          type: "password"
+        },
+        {
+          name: 'age',
+          placeholder: 'Idade',
+          value: (user.age != undefined) ? user.age.toString() : "",
+          type: "number"
+        },
+        {
+          name: 'height',
+          placeholder: 'Altura',
+          value: (user.height != undefined) ? user.height.toString() : "",
+          type: "number"
+        },
+        {
+          name: 'weight',
+          placeholder: 'Peso',
+          value: (user.weight != undefined) ? user.weight.toString() : "",
+          type: "number"
+        },
+        {
+          name: 'goal',
+          placeholder: 'Objetivo',
+          value: user.goal,
+          type: "text"
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Salvar',
+          role: 'save',
+          handler: data => {
+
+            user.name = data.name;
+            user.password = data.password;
+            user.age = data.age;
+            user.height = data.height;
+            user.weight = data.weight;
+            user.goal = data.goal;
+            this.mongoService.put("users", user)
+            .subscribe(() => this.showToast());
+
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
+  showToast() {
+
+    let toast = this.toastCtrl.create({
+      message: "Usuário editado com sucesso!",
+      duration: 2000
+    });
+
+    toast.present();
+  }
 }
